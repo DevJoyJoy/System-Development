b5861cd04f1c1fe94d6c8262876baca00c0e54a2
DevJoyJoy <joycenascimento2007@gmail.com>
2026-03-17 10:51:17 -0300

    Worked just fine

--- products-full/front/src/App.jsx
+++ products-full/front/src/App.jsx
@@ -6,20 +6,24 @@ function App() {
   const [products, setProducts] = useState([])
   const fetchProducts = async () => {
     const response = await axios.get('http://localhost:8080/api/products/products')
-    setProducts(response.data.products)
+    setProducts(response.data)
+    console.log(response.data);
   }
 
   useEffect(() => {
     fetchProducts();
   }, [])
+
   return (
     <>
      {
       products.map((product) => {
         return (
-        <div>
-          <span>{product.name}</span>
-          <span>{product.price}</span>
+        <div className='bg-purple-400'>
+          <div className='bg-purple-200 w-100 h-70 p-3 m-5'>
+            <span>{product.name}</span>
+            <span>{product.price}</span>
+          </div>
         </div>)
       })
      }
--- products-full/products-0303/src/server.ts
+++ products-full/products-0303/src/server.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import connectDB from './database/database.ts';
-
+import routes from './routes/route.ts';
+import cors from 'cors';
 const app = express();
 const port = 8080;
 
 connectDB();
+
+app.use(cors({
+    origin: '*' //o cors precisa ficar antes do routes para as rotas serem declaradas com a configuração dele!! :)
+}))
+
+routes(app)
+
+
+
 app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
